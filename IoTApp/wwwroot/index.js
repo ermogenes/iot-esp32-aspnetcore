68f3c5952c6c7fe81cc2e303f39cce99479096c1
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2023-03-16 14:02:30 -0300

    Refatora projeto

--- IoTApp/wwwroot/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    alert("ok");
-    const response = await fetch('/data');
-    const result = await response.json();
-    console.log(result);
-    document.getElementById('data-from-device').innerText = result.sensor;
-});
